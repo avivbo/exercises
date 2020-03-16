e8a1c95b245849a2e814b07082b8b0a4442188c9
avivbo <avivbo287@gmail.com>
2020-03-16 11:31:19 +0200

    start with array-forEach

---
+++ javascript/ES6/array-forEach.js
@@ -0,0 +1,13 @@
+// Classic for-loop
+
+var colors = [ 'red', 'blue', 'green'];
+
+for (var i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
+
+// ES6 forEach
+colors.forEach(function(color) {
+    console.log(color);
+});
+
